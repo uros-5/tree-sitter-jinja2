7e489cd30989988c6594ccb004e9ebe8c3840856
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2026-06-27 22:47:35 +0200

    add filter_operator and equal operator

--- grammar.js
+++ grammar.js
@@ -21,8 +21,8 @@ export default grammar({
     _node: $ => choice($.statement, $.expression, $.comment, $._text),
 
     statement: $ => seq($.statement_begin, $.keyword, optional($._inner_text), $.statement_end),
-    statement_begin: $ => seq('{%', optional($.white_space_control)),
-    statement_end: $ => seq(optional($.white_space_control), '%}'),
+    statement_begin: $ => /\{%[-+]?/,
+    statement_end: $ => /[-+]?%\}/,
 
     expression: $ => seq($.expression_begin, optional($._inner_text2), $.expression_end),
     expression_begin: $ => '{{',
@@ -35,16 +35,17 @@ export default grammar({
     comment: $ => seq('{#', /[^#]*/, '#}'),
 
     keyword: $ => choice('for', 'in', 'endfor', 'if', 'and', 'or', 'endif', 'else', 'elif', 'raw', 'endraw', 'macro', 'endmacro', 'extends', 'block', 'endblock', 'call', 'endcall', 'filter', 'endfilter', 'set', 'endset', 'include', 'import', 'from', 'autoescape', 'endautoescape', 'trans', 'endtrans', 'pluralize', 'with', 'endwith', 'debug', 'do', 'is'),
-    white_space_control: $ => /[-+]/,
     _white_space: $ => /\s+/,
 
-    _inner_text: $ => repeat1(choice($.keyword, field('identifier', $.dotted_identifier), field('identifier', $.identifier), $._white_space, $.operator, $.string, $.map)),
-    _inner_text2: $ => repeat1(choice(field('identifier', $.dotted_identifier), field('identifier', $.identifier), $._white_space, $.operator, $.string, $.map)),
+    _inner_text: $ => repeat1(choice($.keyword, field('identifier', $.dotted_identifier), field('identifier', $.identifier), $._white_space, $.filter_operator, $.equal_operator, $.operator, $.string, $.map)),
+    _inner_text2: $ => repeat1(choice(field('identifier', $.dotted_identifier), field('identifier', $.identifier), $._white_space, $.filter_operator, $.equal_operator, $.operator, $.string, $.map)),
 
 
     dotted_identifier: $ => seq(field('attribute', $.identifier), repeat1(seq('.', field('attribute', $.identifier)))),
     identifier: $ => /[\w_]+/,
-    operator: $ => /[^\w_{#%}'"]+/,
+    operator: $ => /[^\w_{#%}'"|=\s]+/,
+    filter_operator: $ => "|",
+    equal_operator: $ => "=",
     string: $ => /['"][^'"]*['"]/,
   }
 }) ;
